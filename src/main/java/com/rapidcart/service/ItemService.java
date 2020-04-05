03bbf0348afa3413f8cb50fdf158d110233b583e
nessyphilip7 <59884611+nessyphilip7@users.noreply.github.com>
2020-04-05 10:06:02 +0530

    Item Changes (#10)

--- src/main/java/com/rapidcart/service/ItemService.java
+++ src/main/java/com/rapidcart/service/ItemService.java
@@ -2,43 +2,19 @@ package com.rapidcart.service;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import com.rapidcart.dao.CustomerDao;
-import com.rapidcart.dao.ItemDao;
-import com.rapidcart.model.Customer;
 import com.rapidcart.model.Item;
 
 
-@Service
-public class ItemService {
 
-	@Autowired
-	private ItemDao itemDao;
+public interface ItemService {
+	Long createItem(Item item);
 
-	
-	public Long createItem(Item item) {
-		return itemDao.createItem(item);
-	}
+	Item getItem(String id);
 
-	
-	public Item getItem(String id) {
-		return itemDao.getItem(id);
-	}
+	List<Item> getItems();
 
+	void createOrUpdateItem(Item item);
 
-	public List<Item> getItems() {
-		return itemDao.getItems();
-	}
+	void deleteItem(String id);
 
-
-	public void createOrUpdateItem(Item item) {
-		itemDao.createOrUpdateItem(item);
-	}
-
-
-	public void deleteItem(String id) {
-		itemDao.deleteItem(id);
-	}
 }
---
+++ src/main/java/com/rapidcart/service/ItemServiceImpl.java
@@ -0,0 +1,42 @@
+package com.rapidcart.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rapidcart.dao.ItemDao;
+import com.rapidcart.model.Item;
+
+@Service
+public class ItemServiceImpl implements ItemService{
+
+	@Autowired
+	private ItemDao itemDao;
+	
+	@Override
+	public Long createItem(Item item) {
+		return itemDao.createItem(item);
+	}
+
+	@Override
+	public Item getItem(String id) {
+		return itemDao.getItem(id);
+	}
+
+	@Override
+	public List<Item> getItems() {
+		return itemDao.getItems();
+	}
+
+	@Override
+	public void createOrUpdateItem(Item item) {
+		itemDao.createOrUpdateItem(item);
+	}
+
+	@Override
+	public void deleteItem(String id) {
+		itemDao.deleteItem(id);
+	}
+
+}
